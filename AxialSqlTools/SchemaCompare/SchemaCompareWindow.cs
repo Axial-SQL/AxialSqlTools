1531aafa55cef033d1ac3477ef0cd834b5034c43
Alex Bochkov <albochkov03@gmail.com>
2025-11-14 16:32:35 -0700

    fix window title

--- AxialSqlTools/SchemaCompare/SchemaCompareWindow.cs
+++ AxialSqlTools/SchemaCompare/SchemaCompareWindow.cs
@@ -23,7 +23,7 @@ namespace AxialSqlTools
         /// </summary>
         public SchemaCompareWindow() : base(null)
         {
-            this.Caption = "SchemaCompareWindow";
+            this.Caption = "Database Schema Compare";
 
             // This is the user control hosted by the tool window; Note that, even if this class implements IDisposable,
             // we are not calling Dispose on this object. This is because ToolWindowPane calls Dispose on
