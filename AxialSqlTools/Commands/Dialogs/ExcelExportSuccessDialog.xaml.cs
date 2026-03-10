4b2b795fca89ccd4cca6bab5f60fce39eaa4c9b2
Alex Bochkov <albochkov03@gmail.com>
2026-03-10 17:24:26 -0500

    Add Excel export success dialog with Open in Excel action (#75)

---
+++ AxialSqlTools/Commands/Dialogs/ExcelExportSuccessDialog.xaml.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Windows;
+
+namespace AxialSqlTools
+{
+    public partial class ExcelExportSuccessDialog : Window
+    {
+        private readonly string filePath;
+
+        public ExcelExportSuccessDialog(string filePath)
+        {
+            InitializeComponent();
+
+            this.filePath = filePath ?? string.Empty;
+            FilePathText.Text = this.filePath;
+        }
+
+        private void OpenInExcelButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(filePath) || !File.Exists(filePath))
+            {
+                MessageBox.Show(this, "The exported file could not be found.", "Open in Excel", MessageBoxButton.OK, MessageBoxImage.Warning);
+                return;
+            }
+
+            try
+            {
+                ProcessStartInfo processStartInfo = new ProcessStartInfo
+                {
+                    FileName = filePath,
+                    UseShellExecute = true
+                };
+
+                Process.Start(processStartInfo);
+                Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(this, "Unable to open the exported file in Excel.\n\n" + ex.Message, "Open in Excel", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void OkButton_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
