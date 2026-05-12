82ecc71fe9db675192c63dfb7f491d6adaf58f07
Alex Bochkov <albochkov03@gmail.com>
2026-05-12 09:48:53 -0600

    wip

--- AxialSqlTools/SyncToGitHub/DatabaseScripterToolWindowControl.xaml.cs
+++ AxialSqlTools/SyncToGitHub/DatabaseScripterToolWindowControl.xaml.cs
@@ -8,7 +8,6 @@ using Octokit;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Security.Cryptography;
@@ -26,6 +25,8 @@ namespace AxialSqlTools
 {
     public partial class DatabaseScripterToolWindowControl : UserControl
     {
+        private readonly ToolWindowThemeController _themeController;
+
         // ripped out of Ola script using AI - https://github.com/olahallengren/sql-server-maintenance-solution/
         private string _dbPullQuery = @"
 SET NOCOUNT ON;
@@ -179,8 +180,7 @@ WHERE NOT EXISTS (SELECT 1 FROM @ExcludeNames AS e WHERE e.database_name = i.dat
 
         private void buttonWikiPage_Click(object sender, RequestNavigateEventArgs e)
         {
-            Process.Start(new ProcessStartInfo(e.Uri.AbsoluteUri) { UseShellExecute = true });
-            e.Handled = true;
+            ToolWindowNavigation.HandleRequestNavigate(e);
         }
 
 
@@ -226,11 +226,7 @@ WHERE NOT EXISTS (SELECT 1 FROM @ExcludeNames AS e WHERE e.database_name = i.dat
 
         private void RepoUrlHyperlink_RequestNavigate(object sender, System.Windows.Navigation.RequestNavigateEventArgs e)
         {
-            if (e.Uri != null)
-            {
-                Process.Start(new ProcessStartInfo(e.Uri.AbsoluteUri) { UseShellExecute = true });
-            }
-            e.Handled = true;
+            ToolWindowNavigation.HandleRequestNavigate(e);
         }
 
 
@@ -421,12 +417,18 @@ WHERE NOT EXISTS (SELECT 1 FROM @ExcludeNames AS e WHERE e.database_name = i.dat
         public DatabaseScripterToolWindowControl()
         {
             InitializeComponent();
+            _themeController = new ToolWindowThemeController(this, ApplyThemeBrushResources);
             ProgressListBox.ItemsSource = _progressMessages;
 
             _profiles = new ObservableCollection<GitHubSyncProfile>(ProfileStore.Load());
             ProfilesComboBox.ItemsSource = _profiles;
 
             UpdateMainFrameState();
+        }
+
+        private void ApplyThemeBrushResources()
+        {
+            ToolWindowThemeResources.ApplySharedTheme(this);
         }
 
         private async void RunButton_Click(object sender, RoutedEventArgs e)
