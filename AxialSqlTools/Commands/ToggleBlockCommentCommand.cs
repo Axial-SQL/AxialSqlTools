02d8f03712488c2b9b190eced811ca4c03fdb4ad
blake-goofy <blake13blake@gmail.com>
2026-06-10 22:26:41 -0500

    Refactor block comment logic and update package references

--- AxialSqlTools/Commands/ToggleBlockCommentCommand.cs
+++ AxialSqlTools/Commands/ToggleBlockCommentCommand.cs
@@ -1,6 +1,9 @@
 using System;
+using System.Collections.Generic;
 using System.ComponentModel.Design;
+using System.IO;
 using EnvDTE;
+using Microsoft.SqlServer.TransactSql.ScriptDom;
 using Microsoft.VisualStudio.Shell;
 using Microsoft.VisualStudio.Shell.Interop;
 using Task = System.Threading.Tasks.Task;
@@ -98,30 +101,18 @@ namespace AxialSqlTools
             if (string.IsNullOrEmpty(fullText))
                 return;
 
-            int originalOffset = selection.ActivePoint.AbsoluteCharOffset;
-            int caretIndex = Math.Max(0, Math.Min(fullText.Length, originalOffset - 1));
-            if (!TryFindEnclosingBlockComment(fullText, caretIndex, out int openIndex, out int closeIndex))
+            TextPoint activePoint = selection.ActivePoint;
+            var caret = new TextPosition(activePoint.Line, activePoint.LineCharOffset);
+            if (!TryFindEnclosingBlockComment(fullText, caret, out TextPosition openPosition, out TextPosition closePosition))
                 return;
 
             EditPoint closePoint = textDoc.StartPoint.CreateEditPoint();
-            closePoint.MoveToAbsoluteOffset(closeIndex + 1);
+            closePoint.MoveToLineAndOffset(closePosition.Line, closePosition.Column);
             closePoint.Delete(2);
 
             EditPoint openPoint = textDoc.StartPoint.CreateEditPoint();
-            openPoint.MoveToAbsoluteOffset(openIndex + 1);
+            openPoint.MoveToLineAndOffset(openPosition.Line, openPosition.Column);
             openPoint.Delete(2);
-
-            int restoredOffset = originalOffset;
-            if (caretIndex > closeIndex)
-            {
-                restoredOffset -= 4;
-            }
-            else if (caretIndex > openIndex)
-            {
-                restoredOffset -= 2;
-            }
-
-            selection.MoveToAbsoluteOffset(Math.Max(1, restoredOffset), false);
         }
 
         private bool TryGetOuterBlockCommentDelimiters(string text, out int openIndex, out int closeIndex)
@@ -155,25 +146,167 @@ namespace AxialSqlTools
             return true;
         }
 
-        private bool TryFindEnclosingBlockComment(string text, int caretIndex, out int openIndex, out int closeIndex)
+        private bool TryFindEnclosingBlockComment(
+            string text,
+            TextPosition caret,
+            out TextPosition openPosition,
+            out TextPosition closePosition)
+        {
+            openPosition = default(TextPosition);
+            closePosition = default(TextPosition);
+
+            var parser = new TSql170Parser(initialQuotedIdentifiers: true);
+            IList<ParseError> errors;
+            IList<TSqlParserToken> tokens = parser.GetTokenStream(new StringReader(text), out errors);
+            foreach (TSqlParserToken token in tokens)
+            {
+                if (token.TokenType != TSqlTokenType.MultilineComment || string.IsNullOrEmpty(token.Text))
+                    continue;
+
+                var tokenStart = new TextPosition(token.Line, token.Column);
+                TextPosition tokenEnd = GetPositionAfterText(token.Text, tokenStart);
+                if (!ContainsPosition(tokenStart, tokenEnd, caret))
+                    continue;
+
+                if (!TryGetTextIndexAtPosition(token.Text, tokenStart, caret, out int caretIndex))
+                    continue;
+
+                if (!TryFindEnclosingBlockCommentDelimiters(token.Text, caretIndex, out int openIndex, out int closeIndex))
+                    continue;
+
+                openPosition = GetPositionAtTextIndex(token.Text, tokenStart, openIndex);
+                closePosition = GetPositionAtTextIndex(token.Text, tokenStart, closeIndex);
+                return true;
+            }
+
+            return false;
+        }
+
+        private bool TryFindEnclosingBlockCommentDelimiters(string commentText, int caretIndex, out int openIndex, out int closeIndex)
         {
             openIndex = -1;
             closeIndex = -1;
 
-            int searchStart = Math.Min(caretIndex, text.Length - 1);
-            openIndex = text.LastIndexOf("/*", searchStart, StringComparison.Ordinal);
-            if (openIndex < 0)
+            var blockStarts = new Stack<int>();
+            int bestOpenIndex = -1;
+            int bestCloseIndex = -1;
+
+            for (int i = 0; i < commentText.Length - 1; i++)
+            {
+                if (commentText[i] == '/' && commentText[i + 1] == '*')
+                {
+                    blockStarts.Push(i);
+                    i++;
+                    continue;
+                }
+
+                if (commentText[i] == '*' && commentText[i + 1] == '/' && blockStarts.Count > 0)
+                {
+                    int candidateOpenIndex = blockStarts.Pop();
+                    int candidateCloseIndex = i;
+                    if (candidateOpenIndex <= caretIndex
+                        && caretIndex < candidateCloseIndex + 2
+                        && candidateOpenIndex > bestOpenIndex)
+                    {
+                        bestOpenIndex = candidateOpenIndex;
+                        bestCloseIndex = candidateCloseIndex;
+                    }
+
+                    i++;
+                }
+            }
+
+            if (bestOpenIndex < 0)
                 return false;
 
-            int lastCloseBeforeCaret = text.LastIndexOf("*/", searchStart, StringComparison.Ordinal);
-            if (lastCloseBeforeCaret > openIndex)
-                return false;
-
-            closeIndex = text.IndexOf("*/", openIndex + 2, StringComparison.Ordinal);
-            if (closeIndex < 0 || caretIndex > closeIndex + 2)
-                return false;
-
+            openIndex = bestOpenIndex;
+            closeIndex = bestCloseIndex;
             return true;
+        }
+
+        private bool ContainsPosition(TextPosition start, TextPosition end, TextPosition position)
+        {
+            return ComparePositions(start, position) <= 0 && ComparePositions(position, end) < 0;
+        }
+
+        private int ComparePositions(TextPosition left, TextPosition right)
+        {
+            if (left.Line != right.Line)
+                return left.Line.CompareTo(right.Line);
+
+            return left.Column.CompareTo(right.Column);
+        }
+
+        private bool TryGetTextIndexAtPosition(string text, TextPosition start, TextPosition position, out int index)
+        {
+            TextPosition current = start;
+            for (int i = 0; i <= text.Length; i++)
+            {
+                if (current.Line == position.Line && current.Column == position.Column)
+                {
+                    index = i;
+                    return true;
+                }
+
+                if (i == text.Length)
+                    break;
+
+                AdvancePosition(text, ref i, ref current);
+            }
+
+            index = -1;
+            return false;
+        }
+
+        private TextPosition GetPositionAtTextIndex(string text, TextPosition start, int targetIndex)
+        {
+            TextPosition current = start;
+            for (int i = 0; i < targetIndex && i < text.Length; i++)
+            {
+                AdvancePosition(text, ref i, ref current);
+            }
+
+            return current;
+        }
+
+        private TextPosition GetPositionAfterText(string text, TextPosition start)
+        {
+            return GetPositionAtTextIndex(text, start, text.Length);
+        }
+
+        private void AdvancePosition(string text, ref int index, ref TextPosition position)
+        {
+            if (text[index] == '\r')
+            {
+                if (index + 1 < text.Length && text[index + 1] == '\n')
+                {
+                    index++;
+                }
+
+                position = new TextPosition(position.Line + 1, 1);
+                return;
+            }
+
+            if (text[index] == '\n')
+            {
+                position = new TextPosition(position.Line + 1, 1);
+                return;
+            }
+
+            position = new TextPosition(position.Line, position.Column + 1);
+        }
+
+        private struct TextPosition
+        {
+            public TextPosition(int line, int column)
+            {
+                Line = line;
+                Column = column;
+            }
+
+            public int Line { get; }
+
+            public int Column { get; }
         }
     }
 }
