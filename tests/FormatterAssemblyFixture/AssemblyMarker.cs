533f5516a10258e06a157a4d3c44e935d55807c4
Alex Bochkov <albochkov03@gmail.com>
2026-09-19 16:48:18 -0600

    Delete tests directory

--- tests/FormatterAssemblyFixture/AssemblyMarker.cs
+++
@@ -1,5 +0,0 @@
-// Handwritten assembly-loading fixture, not SSMS code. A different version catches build-time pins.
-namespace FormatterAssemblyFixture
-{
-    public sealed class AssemblyMarker { }
-}
